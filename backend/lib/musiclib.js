c9d730ceffce92cecd51bb89d23011042b7b6cfe
Suryaa <0x0is1@protonmail.com>
2024-08-22 00:02:36 +0530

    Unbelievable i really tried to change constant variable

--- backend/lib/musiclib.js
+++ backend/lib/musiclib.js
@@ -94,7 +94,7 @@ class ServiceProvider {
     async playById(id) {
         const songDetails = await this.request(`song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=${id}`);
         const token = songDetails[id].encrypted_media_url;
-        const authTokenData = await this.request(`song.generateAuthToken&url=${encodeURIComponent(token)}&bitrate=160&api_version=4&_format=json&ctx=web6dot0&_marker=0`);
+        var authTokenData = await this.request(`song.generateAuthToken&url=${encodeURIComponent(token)}&bitrate=160&api_version=4&_format=json&ctx=web6dot0&_marker=0`);
         authTokenData.auth_url = authTokenData.auth_url.replace("ac.cf.", "aac.")
         const result = {
             ...songDetails[id],
