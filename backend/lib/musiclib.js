0ee4ab82361bae7b25ed67fe1815ca41d7e7f43a
Suryaa <0x0is1@protonmail.com>
2024-08-22 00:01:46 +0530

    Different timezone server fix

--- backend/lib/musiclib.js
+++ backend/lib/musiclib.js
@@ -95,7 +95,7 @@ class ServiceProvider {
         const songDetails = await this.request(`song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=${id}`);
         const token = songDetails[id].encrypted_media_url;
         const authTokenData = await this.request(`song.generateAuthToken&url=${encodeURIComponent(token)}&bitrate=160&api_version=4&_format=json&ctx=web6dot0&_marker=0`);
-
+        authTokenData.auth_url = authTokenData.auth_url.replace("ac.cf.", "aac.")
         const result = {
             ...songDetails[id],
             "streamurl": authTokenData
